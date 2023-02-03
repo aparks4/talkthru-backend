a19f37c777cd309160ed9925e68ecc73d447c61a
Peter Yoo <trustintofu@gmail.com>
2023-02-02 18:42:59 -0800

    feat(index): initializes the express instance and http server.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import http from 'http';
 
 dotenv.config();
-const app: Express = express();
-
 const port = process.env.PORT || 8080;
+const app: Express = express();
+const server = http.createServer(app);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send(`Server running at port: ${port}`);
+  res.send(`Listening to the server on ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+server.listen(port, () => {
+  console.log(`Listening to the server on ${port}`);
 });
