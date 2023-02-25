dd0729edeadef767c6e5ac62d332cbcd8fab242b
Peter Yoo <trustintofu@gmail.com>
2023-02-24 16:35:50 -0800

    refactor(login/index): Hides secret key in .env.

--- src/login/index.ts
+++ src/login/index.ts
@@ -7,6 +7,8 @@ import { IUser } from '../../types/types';
 const router = express.Router();
 const prisma = new PrismaClient();
 
+const SECRET_KEY = process.env.JWT_SECRET_KEY as string;
+
 // POST route for user login
 router.post('/', async (req: Request, res: Response) => {
   const { email, password }: IUser = req.body;
@@ -23,7 +25,7 @@ router.post('/', async (req: Request, res: Response) => {
       res.status(401).send('Invalid email or password');
       return;
     }
-    const token = jwt.sign({ userId: user.id }, 'your_secret_key');
+    const token = jwt.sign({ userId: user.id }, SECRET_KEY);
     res.status(200).json({ token });
   } catch (error) {
     console.log(error);
