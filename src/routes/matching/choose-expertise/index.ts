0e7013cd1d3d2eb61ff40e3fb3fb5a8306d2fca6
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-03-01 11:04:04 -0800

    refactor(matching): Refactors routes so that each field is updated individually and matching users is also handled in it's own route.

--- src/routes/matching/choose-expertise/index.ts
+++ src/routes/matching/choose-expertise/index.ts
@@ -4,122 +4,70 @@ import { PrismaClient } from '@prisma/client';
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// First create a map for expertise levels
-const expertiseMap = new Map();
-
-expertiseMap.set('Beginner', 1);
-expertiseMap.set('Intermediate', 2);
-expertiseMap.set('Advanced', 3);
-
 router.get('/', async (req: Request, res: Response) => {
-  req;
-  console.log(expertiseMap);
+	req;
 
-  const allUsersMatching = await prisma.user.findMany({
-    where: {
-      matching: true,
-    },
-  });
+	const allUsersMatching = await prisma.user.findMany({
+		where: {
+			matching: true,
+		},
+	});
 
-  allUsersMatching.forEach((user) => {
-    console.log(user);
-  });
+	allUsersMatching.forEach((user) => {
+		console.log(user);
+	});
 
-  res.send('Choose Expertise Endpoint');
+	res.send('Choose Expertise Endpoint');
 });
 
 // Update route to update expertise
 router.put('/', async (req: Request, res: Response) => {
-  const subject = req.body.subject;
-  const expertise = req.body.expertise;
-  const id = parseInt(req.body.id);
+	const expertise = req.body.expertise;
+	const id = parseInt(req.body.id);
 
-  if (!subject) {
-    res.status(400).json({ message: 'subject is empty' });
-    return;
-  }
+	if (!expertise) {
+		res.status(400).json({ message: 'expertise is empty' });
+		return;
+	}
 
-  if (!expertise) {
-    res.status(400).json({ message: 'expertise is empty' });
-    return;
-  }
+	if (!id) {
+		res.status(400).json({ message: 'User id is empty' });
+		return;
+	}
 
-  if (!id) {
-    res.status(400).json({ message: 'User id is empty' });
-    return;
-  }
+	try {
+		const user = await prisma.user.findUnique({ where: { id } });
 
-  try {
-    const user = await prisma.user.findUnique({ where: { id } });
+		if (!user) {
+			res.status(404).json({ message: 'User not found' });
+			return;
+		}
 
-    if (!user) {
-      res.status(404).json({ message: 'User not found' });
-      return;
-    }
+		const updatedExpertise = await prisma.user.update({
+			where: { id },
+			data: { expertise },
+		});
 
-    const updatedExpertise = await prisma.user.update({
-      where: { id },
-      data: { expertise },
-    });
+		if (!updatedExpertise) {
+			res.status(404).json({ message: 'expertise was unable to be updated.' });
+			return;
+		}
 
-    if (!updatedExpertise) {
-      res.status(404).json({ message: 'expertise was unable to be updated.' });
-      return;
-    }
+		const updatedMatching = await prisma.user.update({
+			where: { id },
+			data: { matching: true },
+		});
 
-    const updatedMatching = await prisma.user.update({
-      where: { id },
-      data: { matching: true },
-    });
+		if (!updatedMatching) {
+			res.status(404).json({ message: 'could not update matching to true' });
+			return;
+		}
 
-    if (!updatedMatching) {
-      res.status(404).json({ message: 'could not update matching to true' });
-      return;
-    }
-
-    // Iterate through all users, see who is matching.
-    const allUsersMatching = await prisma.user.findMany({
-      where: {
-        matching: true,
-        subject,
-      },
-    });
-
-    // Keep track of the order to search by
-    let que = [1, 2, 3];
-
-    if (expertiseMap.get(expertise) === 3) {
-      // Searches: 3 -> 2 -> 1
-      que = que.reverse();
-    } else if (expertiseMap.get(expertise) === 2) {
-      // Searches: 2 -> 1 -> 3
-      [que[0], que[1]] = [que[1], que[0]];
-    }
-
-    let matchingUser = null;
-
-    for (const currLevel of que) {
-      for (const user of allUsersMatching) {
-        const userLevel = expertiseMap.get(user.expertise);
-        if (userLevel === currLevel && user.id !== id) {
-          matchingUser = user;
-          break;
-        }
-      }
-      if (matchingUser) {
-        break;
-      }
-    }
-
-    if (matchingUser) {
-      res.status(200).json(matchingUser);
-    } else {
-      res.status(404).json({ message: 'Could not find a match' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Choose expertise put route error.' });
-  }
+		res.status(200).json(updatedMatching);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Choose expertise put route error.' });
+	}
 });
 
 export const chooseExpertiseRouter = router;
